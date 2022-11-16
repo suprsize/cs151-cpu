dfb8f3ea52b50baca3b8b1e2069b50b5a5dba1ac
suprsize <mohammadjami25@gmail.com>
2022-11-15 19:08:55 -0800

    small edit in imm_gen

--- hardware/src/riscv_core/imm_gen.v
+++ hardware/src/riscv_core/imm_gen.v
@@ -23,7 +23,7 @@ module imm_gen #(
         S_TYPE: out = { {20{inst[31]}}, inst[31:25], inst[11:7] };
         B_TYPE: out = { {20{inst[31]}}, inst[7], inst[30:25], inst[11:8], 1'b0 };
         U_TYPE: out = { inst[31:12], 12'b0 };
-        J_TYPE: out = { {10{inst[31]}}, inst[19:12], inst[20], inst[30:21], 1'b0 };
+        J_TYPE: out = { {12{inst[31]}}, inst[19:12], inst[20], inst[30:21], 1'b0 };
         C_TYPE: out = { {27{inst[19]}}, inst[19:15] };
         default: out = 'd0;
       endcase

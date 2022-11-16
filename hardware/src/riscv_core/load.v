73dee3ad24246a74c7e4d5edf9aca0a6fbb148b6
suprsize <mohammadjami25@gmail.com>
2022-11-15 22:20:34 -0800

    added load and stores for RAMs

---
+++ hardware/src/riscv_core/load.v
@@ -0,0 +1,38 @@
+module load #(
+    parameter W_SIZE = 32
+) (
+    input [W_SIZE-1:0] mem_data,
+    input [15:0] addr,
+    input [2:0] func3,
+    input [6:0] opcode,
+    output [W_SIZE-1:0] load_data
+);
+    localparam LOAD_OPCODE = 7'd3;
+    localparam LB_FUNC3 = 3'd0;
+    localparam LH_FUNC3 = 3'd1;
+    localparam LW_FUNC3 = 3'd2;  
+    localparam LBU_FUNC3 = 3'd4;
+    localparam LHU_FUNC3 = 3'd5;  
+
+    reg [W_SIZE-1:0] out;
+    wire [W_SIZE-1:0] shifted_data;
+    wire [4:0] bytes_to_shift;
+
+    assign load_data = opcode == LOAD_OPCODE? out : mem_data;
+    assign bytes_to_shift = 8 * addr[1:0];
+    assign shifted_data = mem_data >> bytes_to_shift;
+ 
+
+    always @(*) begin
+      case(func3)
+        LB_FUNC3:  out = { {24{shifted_data[7]}}, shifted_data[7:0] };
+        LH_FUNC3:  out = { {16{shifted_data[15]}}, shifted_data[15:0]};
+        LW_FUNC3:  out = mem_data;
+        LBU_FUNC3: out = {24'b0, shifted_data[7:0]};
+        LHU_FUNC3: out = {16'b0, shifted_data[15:0]};
+        default:   out = mem_data;
+      endcase
+    end
+
+
+endmodule
---
+++ hardware/src/riscv_core/store.v
@@ -0,0 +1,38 @@
+module store #(
+    parameter W_SIZE = 32
+) (
+    input [W_SIZE-1:0] din,
+    input [15:0] addr,
+    input [2:0] func3,
+    input [6:0] opcode,
+    output [W_SIZE-1:0] store_data,
+    output [3:0] MemRW4
+);
+    localparam STORE_OPCODE = 7'h23; // 23 in hex
+    localparam SB_FUNC3 = 3'd0;
+    localparam SH_FUNC3 = 3'd1;
+    localparam SW_FUNC3 = 3'd2;  
+
+
+    reg [W_SIZE-1:0] out;
+    reg [3:0] mask;
+    wire [W_SIZE-1:0] shifted_data;
+    wire [4:0] bytes_to_shift;
+
+    assign bytes_to_shift = 8 * addr[1:0];
+    assign shifted_data = din >> bytes_to_shift;
+
+    assign store_data = func3 == SW_FUNC3? shifted_data : din;    
+    assign MemRW4 = opcode == STORE_OPCODE? mask : 4'd0;
+
+    always @(*) begin
+      case(func3)
+        SB_FUNC3:  mask = 4'b0001;
+        SH_FUNC3:  mask = 4'b0011;
+        SW_FUNC3:  mask = 4'b1111;
+        default:   mask = 4'd0;
+      endcase
+    end
+
+
+endmodule

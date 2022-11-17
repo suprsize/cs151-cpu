492f99c971c491b028da49307b8f17862dd59030
suprsize <mohammadjami25@gmail.com>
2022-11-16 20:24:11 -0800

    added load and store to the diagram and added a file for control_logic

---
+++ hardware/src/riscv_core/control_logic.v
@@ -0,0 +1,43 @@
+module control_logic #(
+    parameter W_SIZE = 32
+) (
+    input [W_SIZE-1:0] inst_fd, inst_xm, inst_w,
+    input BrEq, BrLt,
+    output PCSel, RegWEn, BrYn, ASel, BSel, MemRW, 
+    output [3:0] ALUSel,
+    output [2:0] ImmSel, 
+    output [2:0] WBSel
+);
+    localparam 
+    ADD = 4'b0_000,
+    SUB = 4'b1_000,
+    AND = 4'b0_111,
+    OR  = 4'b0_110,
+    XOR = 4'b0_100,
+    SLL = 4'b0_001,
+    SRL = 4'b0_101,
+    SRA = 4'b1_101,
+    SLT = 4'b0_010,
+    SLTU= 4'b0_011;
+
+    reg [W_SIZE-1:0] out;
+    assign result = out;
+
+    always @(*) begin
+      case(ALUSel)
+        ADD:    out = a + b;
+        SUB:    out = a - b;
+        AND:    out = a & b;
+        OR:     out = a | b;
+        XOR:    out = a ^ b;
+        SLL:    out = a << b[4:0];
+        SRL:    out = a >> b[4:0];
+        SRA:    out = $signed(a) >>> b[4:0];
+        SLT:    out = $signed(a) < $signed(b) ? 'd1 : 'd0;
+        SLTU:   out = a < b ? 'd1 : 'd0;
+        default: out = b; // Need to only pass b for the lui instruction
+      endcase
+    end
+
+
+endmodule

24d71d0172b937eefc16d6f7414cc889260135de
suprsize <mohammadjami25@gmail.com>
2022-11-17 15:42:30 -0800

    re-wrote branch comp module

--- hardware/src/riscv_core/branch_comp.v
+++ hardware/src/riscv_core/branch_comp.v
@@ -6,10 +6,33 @@ module branch_comp #(
     input [2:0] BrSel,
     output BrTaken
 );
+    localparam 
+    BEQ     = 3'd0,
+    BNE     = 3'd3,
+    BLT     = 3'd4,
+    BLTU    = 3'd6,
+    BGE     = 3'd5,
+    BGEU    = 3'd7;
+
 
     wire BrUn, BrEq, BrLt;
-    assign = BrSel == 3'd6 || BrSel == 3'd7;
+    assign BrUn = (BrSel == BLTU || BrSel == BGEU);
     assign BrEq = BrUn? a == b  : $signed(a) == $signed(b);
     assign BrLt = BrUn? a < b   : $signed(a) < $signed(b);
+
+    reg branch;
+    assign BrTaken = branch;
+    
+    always @(*) begin
+        case(BrSel)
+            BEQ: branch = BrEq;
+            BNE: branch = !BrEq;
+            BLT,
+            BLTU: branch = BrLt;
+            BGE, 
+            BGEU: branch = !BrLt & BrEq;
+            default: branch = 1'd0;
+        endcase
+    end
 
 endmodule
